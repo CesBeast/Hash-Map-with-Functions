262fc3b8a784156e5616aa1bd27f4293530fc6c5
Cesar Garcia <cesbeast@gmail.com>
2015-04-12 11:26:23 -0700

    Create TestCases.java

---
+++ TestCases.java
@@ -0,0 +1,19 @@
+
+public class TestCases {
+
+	public static void main(String[] args) {
+		
+	              WordCount  wcounter = new WordCount("appleout2.txt");
+	              wcounter.start();
+	        //The next 4 lines could be in any order and the code should produce correct results.
+	             wcounter.printHashTableOrder();
+	              wcounter.printAlphaOrder();
+	              wcounter.printFrequencyOrder();
+	              wcounter.printTopTen();
+	              
+	      }
+
+
+	}
+
+
